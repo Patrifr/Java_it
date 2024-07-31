6d29dda0128e306d5c921907c97f8a904f6d84cb
Patri <patriciafornielesro@gmail.com>
2024-07-31 10:39:55 +0200

    Correccion tasca7

--- SPRINT1/TASCA_7/N1/src/Ex1_Employers/EmployeeOnSite.java
+++ SPRINT1/TASCA_7/N1/src/Ex1_Employers/EmployeeOnSite.java
@@ -3,7 +3,7 @@ package Ex1_Employers;
 public class EmployeeOnSite extends Employee {
     private static int gasoline;
 
-    public EmployeeOnSite(String name, String lastname, double hourPrice, int gasoline) {
+    public EmployeeOnSite(String name, String lastname, double hourPrice) {
         super(name, lastname, hourPrice);
     }
 
--- SPRINT1/TASCA_7/N1/src/Ex1_Employers/Main.java
+++ SPRINT1/TASCA_7/N1/src/Ex1_Employers/Main.java
@@ -3,7 +3,7 @@ package Ex1_Employers;
 public class Main {
     public static void main(String[] args) {
         double hours = 0d;
-        EmployeeOnSite employee1 = new EmployeeOnSite("Josep","Alvarez",10.44,35);
+        EmployeeOnSite employee1 = new EmployeeOnSite("Josep","Alvarez",10.44);
         EmployeeOnline employee2 = new EmployeeOnline("Maria","Bergua",10.44);
 
         System.out.println("This month's salary for worker " + employee1.getName() + " " + employee1.getLastname() +
